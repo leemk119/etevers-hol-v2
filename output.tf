c84be30615e7573f7bc430020344088e447e6930
leemk119 <leemk119@gmail.com>
2024-10-30 11:16:58 +0900

    Edit output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+## 1-phase 배포 ##
 # VPC ID 출력
 output "vpc_id" {
   description = "ID of the VPC"
@@ -10,23 +11,26 @@ output "nat_gateway_public_ip" {
   value       = aws_nat_gateway.prod_nat_gw.public_ip
 }
 
-# ALB DNS 이름 출력
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.prod_alb.dns_name
-}
+## 3-phase 배포시 주석 제거 ##
+# # ALB DNS 이름 출력
+# output "alb_dns_name" {
+#   description = "DNS name of the Application Load Balancer"
+#   value       = aws_lb.prod_alb.dns_name
+# }
 
-# 웹 서버 인스턴스 ID 출력
-output "web_instance_ids" {
-  description = "IDs of the web server instances"
-  value       = aws_instance.web[*].id
-}
+## 4-phase 배포시 주석 제거 ##
+# # 웹 서버 인스턴스 ID 출력
+# output "web_instance_ids" {
+#   description = "IDs of the web server instances"
+#   value       = aws_instance.web[*].id
+# }
 
-# RDS 엔드포인트 출력
-output "rds_endpoint" {
-  description = "Endpoint of the RDS instance"
-  value       = aws_db_instance.prod_db.endpoint
-}
+## 5-phase 배포시 주석 제거 ##
+# # RDS 엔드포인트 출력
+# output "rds_endpoint" {
+#   description = "Endpoint of the RDS instance"
+#   value       = aws_db_instance.prod_db.endpoint
+# }
 
 
 
